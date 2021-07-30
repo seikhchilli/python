31efed8833b36a4a1b3d20006d1e974c17bc11d0
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-07-30 12:14:05 +0530

    first commit

---
+++ DisplayTime.py
@@ -0,0 +1,4 @@
+seconds = eval(input("Enter seconds: "))
+minutes = seconds // 60
+remMinutes = seconds % 60;
+print(seconds, "Seconds is",minutes, "minutes and ", remMinutes, " remaining seconds")
---
+++ circle_area.py
@@ -0,0 +1,3 @@
+radius = eval(input("Enter radius of circle: "))
+area = 3.14 * radius * radius
+print(area)

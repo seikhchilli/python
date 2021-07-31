3311ad4a958d769cbab30786d1dc7df2563683db
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-07-31 10:05:31 +0530

    round and format decimals

---
+++ round_nd_format_decimals.py
@@ -0,0 +1,6 @@
+amount = 12618.98
+interest_rate = 0.0013
+interest = amount * interest_rate
+print("interest is: ", interest)
+print("\ninterest is: ", round(interest, 2))
+print("\ninterest is: ", format(interest, ".2f"))

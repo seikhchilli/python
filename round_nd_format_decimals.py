4ac12c233bd6620f94ca0c4efbb302b779da0a52
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-07-31 10:19:01 +0530

    format specifiers

--- round_nd_format_decimals.py
+++ round_nd_format_decimals.py
@@ -3,4 +3,14 @@ interest_rate = 0.0013
 interest = amount * interest_rate
 print("interest is: ", interest)
 print("\ninterest is: ", round(interest, 2))
-print("\ninterest is: ", format(interest, ".2f"))
+print("\ninterest is: ", format(interest, ".2f"))           #format(item, format-specifier)
+
+
+#format-specifier
+"""
+    width.precisionf: format(52.7, 10.2f)           [size = 10 , precision after decimal = 2]
+
+    %: format(0.054, 10.2%)                         [10.2% causes number to be multiplied by 100 and displayed as percentage]
+
+    
+"""

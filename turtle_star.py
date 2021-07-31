53eab4f9148f5f5cf3e8d4f2373b1c6d9e2d55d6
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-07-31 10:56:19 +0530

    turtle star

---
+++ turtle_star.py
@@ -0,0 +1,21 @@
+import turtle
+
+turtle.Screen().bgcolor("black")
+turtle.color("red")
+
+turtle.penup()
+turtle.home()
+turtle.goto(-100, 0)
+turtle.setheading(0)
+turtle.pendown()
+turtle.forward(200)
+turtle.rt(144)
+turtle.forward(200)
+turtle.rt(144)
+turtle.forward(200)
+turtle.rt(144)
+turtle.forward(200)
+turtle.rt(144)
+turtle.forward(200)
+turtle.hideturtle()
+turtle.done()

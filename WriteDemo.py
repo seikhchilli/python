0f7d3184988e52b963454d46f35ca15c1217c6f4
seikhchilli <saurabhkrsingh2001@gmail.com>
2022-01-23 12:09:52 +0530

    Writing into a file

---
+++ WriteDemo.py
@@ -0,0 +1,12 @@
+def main():
+    #open file for output
+    outfile = open("PrimeMinisters.txt", "w")
+
+    #write data to the file
+    outfile.write("Narendra Modi\n")
+    outfile.write("Manmohan Singh\n")
+    outfile.write("Atal Bihari Vajpayee\n")
+
+    outfile.close() #Close the output file
+
+main()

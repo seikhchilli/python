0d72b0a5560a1bbe1f0c8d8e3778ebe3f8443067
seikhchilli <saurabhkrsingh2001@gmail.com>
2022-01-23 11:30:17 +0530

    check if it is valid ssn

--- CheckSSN_string.py
+++ CheckSSN_string.py
@@ -2,11 +2,8 @@ def CheckSSN(ssn):
     if len(ssn) != 9:
         return False
 
-    for i in ssn:
-        if(i >= '0' and i <= '9'):
-            continue
-        else:
-            return False
+    if not ssn.isdigit():
+        return False
         
     return True
 
@@ -14,7 +11,7 @@ def main():
     ssn = input("Enter your Social Security Number: ")
 
     if CheckSSN(ssn):
-        print("Valid SSN")
+        print("Valid SSN") 
     else:
         print("Invalid SSN")
 
